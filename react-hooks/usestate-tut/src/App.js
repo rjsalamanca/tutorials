6ee56708e4782a9a5dd4f1025fd8833c51ea1145
RJ Salamanca <salamancaronito@gmail.com>
2020-03-31 14:22:26 -0400

    using useState with a login example

--- react-hooks/usestate-tut/src/App.js
+++ react-hooks/usestate-tut/src/App.js
@@ -6,13 +6,25 @@ function App() {
       count2: 20
    });
 
+   const [email, setEmail] = useState('');
+   const [password, setPassword] = useState('');
+
+
    return (
       <div>
-         <button onClick={() => setCount(currentState => ({ ...currentState, count: currentState.count + 1 }))} >+</button>
-         <div>Count 1:{count}</div>
-         <button onClick={() => setCount(currentState => ({ ...currentState, count2: currentState.count2 + 1 }))} >+</button>
-         <div>Count 2:{count2}</div>
-      </div>
+         <div style={{ border: '1px solid black', margin: '5px' }}>
+            <h3>Counter Example:</h3>
+            <button onClick={() => setCount(currentState => ({ ...currentState, count: currentState.count + 1 }))} >+</button>
+            <div>Count 1:{count}</div>
+            <button onClick={() => setCount(currentState => ({ ...currentState, count2: currentState.count2 + 1 }))} >+</button>
+            <div>Count 2:{count2}</div>
+         </div>
+         <div style={{ border: '1px solid black', margin: '5px' }}>
+            <h3>Login Example:</h3>
+            <input name="email" value={email} onChange={e => setEmail(e.target.value)} />
+            <input type="password" name="password" value={password} onChange={e => setPassword(e.target.value)} />
+         </div>
+      </div >
    );
 }
 
