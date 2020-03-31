d31b10d41a2a6a7544d1f60a00d225927a097bc8
RJ Salamanca <salamancaronito@gmail.com>
2020-03-31 14:15:01 -0400

    working with an object and useState

--- react-hooks/usestate-tut/src/App.js
+++ react-hooks/usestate-tut/src/App.js
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
 
 function App() {
-   const [count, setCount] = useState(10);
+   const [{ count, count2 }, setCount] = useState({
+      count: 10,
+      count2: 20
+   });
 
    return (
       <div>
-         <button onClick={() => setCount(count + 1)} >+</button>
-         <button onClick={() => setCount(count - 1)} >-</button>
-         <div>{count}</div>
+         <button onClick={() => setCount(currentState => ({ ...currentState, count: currentState.count + 1 }))} >+</button>
+         <div>Count 1:{count}</div>
+         <button onClick={() => setCount(currentState => ({ ...currentState, count2: currentState.count2 + 1 }))} >+</button>
+         <div>Count 2:{count2}</div>
       </div>
    );
 }
