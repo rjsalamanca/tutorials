8844ee7922da0f5ccd3369da934386060f2bc50b
RJ Salamanca <salamancaronito@gmail.com>
2020-03-31 12:44:22 -0400

    added pages installed react-router-dom created a nav

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Index } from "./pages/index";
+import { About } from "./pages/about"
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <Router>
+         <div>
+            <nav>
+               <ul>
+                  <li>
+                     <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                     <Link to="/about">About</Link>
+                  </li>
+               </ul>
+            </nav>
+
+            <Route path="/" exact component={Index} />
+            <Route path="/about/" component={About} />
+         </div>
+      </Router>
+   );
 }
 
 export default App;
